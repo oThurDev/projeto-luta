2adbb8f4f2a58e5ef8266c2cb8d666be681675cf
arthurbcg <arthurbcg16@gmail.com>
2023-07-27 16:14:20 -0300

    finish-set

--- js/classes.js
+++ js/classes.js
@@ -14,7 +14,7 @@ class Character {
     }
 
     set life (newLife) {
-        this._life = new Life < 0 ? 0 : newLife;
+        this._life = newLife < 0 ? 0 : newLife;
     }
 }
 
@@ -59,5 +59,41 @@ class BigMonster extends Character {
         this.attack = 18;
         this.defense = 8;
         this.maxLife = this.life;
+    }
+}
+
+//criando o cenario
+class Stage {
+    constructor(fighter1, fighter2, fighter1El, fighter2El) {
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+        this.fighter1El = fighter1El;
+        this.fighter2El = fighter2El;
+    }
+
+    startGame() {
+        this.update();
+
+        //botao de atacar
+        this.fighter1El.querySelector('.attack-button').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2))
+        this.fighter2El.querySelector('.attack-button').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1))
+    }
+
+    update() {
+        //fighter1
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP`
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100
+        this.fighter1El.querySelector('.life').style.width = `${f1Pct}%`;
+
+        //fighter2
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP`
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100
+        this.fighter2El.querySelector('.life').style.width = `${f2Pct}%`;
+    }
+
+    doAttack(attacking, attacked) {
+        console.log(`${attacking.name} esta atacando ${attacked.name}`)
+
+        this.update()
     }
 }
