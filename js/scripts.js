da4f1c94bb4cb8a48d16aa30182f90262c620b02
arthurbcg <arthurbcg16@gmail.com>
2023-07-27 16:54:52 -0300

    finish-log

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 let char = new Knight('Arthur')
 let monster = new Sorcerer('Bruno')
 
+let log = new Log(document.querySelector('.log'))
+
 const stage = new Stage (
     char,
     monster,
     document.querySelector('#char'),
-    document.querySelector('#monster')
+    document.querySelector('#monster'),
+    log
 )
 
 stage.startGame();
